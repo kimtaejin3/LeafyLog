231fdbafb94fe548c1d9c6c67ed859d91cb49845
swimjiy <swimjiy@gmail.com>
2023-08-18 21:35:19 +0900

    color 리뷰 추가

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -14,6 +14,7 @@ export default function Btn({ style, color, text }: Props) {
   );
 }
 
+// REVIEW: styles에 primary, secondary 컬러를 정해놓으신 걸 봤는데 중간중간에 아래와 같이 컬러값이 static하게 들어가는 모습이 보이는데, 혹시 이렇게 작업하신 이유가 있으실까요? 그냥 궁금해서 여쭤봅니다!
 const Button = styled.button`
   background-color: ${(props) =>
     props.color === "green" ? "#ACEB44" : "#A67EFA"};
