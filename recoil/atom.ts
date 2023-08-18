6d0a32ccb3720764552f060ebbf22203efb281a3
swimjiy <swimjiy@gmail.com>
2023-08-18 21:36:30 +0900

    atom 리뷰 추가

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 import { atom } from "recoil";
 
-//atom에서 day.js를 쓰는 게 이상한건 아닐지..
+//atom에서 day.js를 쓰는 게 이상한건 아닐지.. -> REVIEW: default에 쓰는거라 크게 상관 없습니다!
 const day = dayjs();
 
 export const yearState = atom({
